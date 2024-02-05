9912c4187710dbea5c7db5e7acb23e4777d48d35
Arfan <arfandnl@gmail.com>
2024-02-05 22:15:51 +0800

    Update mediumDataStorage.py

--- mediumDataStorage.py
+++ mediumDataStorage.py
@@ -4,7 +4,7 @@ import os
 
 # Initialize the SD card
 spi=SPI(1,baudrate=40000000,sck=Pin(10),mosi=Pin(11),miso=Pin(12))
-sd=sdcard.SDCard(spi,Pin(13))
+sd=sdcard.SDCard(spi,cs=Pin(13))
 
 # Create a instance of MicroPython Unix-like Virtual File System (VFS),
 vfs=os.VfsFat(sd)
