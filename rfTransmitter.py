31a6fe6366994c046f9b047a7f4d7aa6a3b20560
yxxyz <outtaleovaldez@yahoo.com>
2024-02-07 15:50:08 +0800

    changes to RF file
    Edited the RF python file code to suit our application for the exchange of data

--- rfTransmitter.py
+++ rfTransmitter.py
@@ -27,7 +27,7 @@ cfg = {"spi": spi, "miso": 4, "mosi": 7, "sck": 6, "csn": 5, "ce": 8}
 pipes = (b"\xe1\xf0\xf0\xf0\xf0", b"\xd2\xf0\xf0\xf0\xf0")
 
 
-def initiator():
+def initiator(rfMssg):
     csn = Pin(cfg["csn"], mode=Pin.OUT, value=1)
     ce = Pin(cfg["ce"], mode=Pin.OUT, value=0)
     spi = cfg["spi"]
@@ -37,21 +37,25 @@ def initiator():
     nrf.open_rx_pipe(1, pipes[1])
     nrf.start_listening()
 
+    # Variable to keep track of how many trials
     num_needed = 16
-    num_successes = 0
     num_failures = 0
-    led_state = 0
 
-    print("NRF24L01 initiator mode, sending %d packets..." % num_needed)
+    print("NRF24L01 initiator mode, %d attempts" % num_needed)
 
-    while num_successes < num_needed and num_failures < num_needed:
+    while num_failures < num_needed:
         # stop listening and send packet
         nrf.stop_listening()
-        mssg = "cibai"
+
+        # Assign the message to a local variable
+        mssg = rfMssg
         print("sending:", mssg)
+        
+        # Send the message through RF
         try:
             nrf.send(mssg.encode('utf-8'))
         except OSError:
+            # Commonly hardware error
             pass
 
         # start listening again
@@ -60,60 +64,29 @@ def initiator():
         # wait for response, with 250ms timeout
         start_time = utime.ticks_ms()
         timeout = False
+
+        # Track the time to wait for a response for 250ms
         while not nrf.any() and not timeout:
             if utime.ticks_diff(utime.ticks_ms(), start_time) > 250:
                 timeout = True
 
+        # Check if got a response from the responder or not
+        # Did not get a response
         if timeout:
             print("failed, response timed out")
             num_failures += 1
 
+        # Obtained a response
         else:
             # recv packet
             pingedMssg = nrf.recv().decode('utf-8')
 
-            # print response and round-trip delay
-            num_successes += 1
+            print("Pinged Message: ", pingedMssg)
+
+            # Break the loop for transmitting since responder got the message
+            break
 
         # delay then loop
         utime.sleep_ms(250)
 
-    print("initiator finished sending; successes=%d, failures=%d" % (num_successes, num_failures))
-
-def responder():
-    csn = Pin(cfg["csn"], mode=Pin.OUT, value=1)
-    ce = Pin(cfg["ce"], mode=Pin.OUT, value=0)
-    spi = cfg["spi"]
-    nrf = NRF24L01(spi, csn, ce, payload_size=8)
-
-    nrf.open_tx_pipe(pipes[1])
-    nrf.open_rx_pipe(1, pipes[0])
-    nrf.start_listening()
-
-    print("NRF24L01 responder mode, waiting for packets... (ctrl-C to stop)")
-
-    while True:
-        # Check if any data is received
-        if nrf.any():
-            # While data has been received
-            while nrf.any():
-                buf = nrf.recv()
-                mssg = buf.decode('utf-8')
-                print("received:", mssg)
-                utime.sleep_ms(_RX_POLL_DELAY)
-
-            # Give initiator time to get into receive mode.
-            utime.sleep_ms(_RESPONDER_SEND_DELAY)
-
-            # Stop boat RF from listening
-            nrf.stop_listening()
-
-            # Try replying to the message to the initiator
-            try:
-                pingMssg = "Got it!"
-                nrf.send(pingMssg.encode('utf-8'))
-            except OSError:
-                pass
-            
-            print("sent response")
-            nrf.start_listening()
+    print("initiator finished sending; attempts = %d" % (num_failures+1))
